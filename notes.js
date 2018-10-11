c118c89de78918bd1fe57366d441df39231e986a
sinamirzahosseinian <sina.mirza2@gmail.com>
2018-10-11 10:40:26 +0330

    2-added

--- notes.js
+++ notes.js
@@ -1,4 +1,9 @@
-1.way of cloning an array:
-const theArray = [sheep1 , sheep2 , sheep3 ]
-old way : const cloneArray = theArray.slice()
-new way : const cloneArray = [...theArray]
+// 1.way of cloning an array:
+// const theArray = [sheep1 , sheep2 , sheep3 ]
+// old way : const cloneArray = theArray.slice()
+// new way : const cloneArray = [...theArray]
+
+// 2.Search for a value in an Array
+//     some : return a boolean
+//     find : return the matched value
+//     findIndex :return the index of matched value
